5f9183c7277133f453e14262d56f0a454827d59a
Shadrack <skmutai6@gmail.com>
2024-05-27 22:51:43 +0300

    commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value = ''):
+        self.value = value if isinstance(value, str) else ValueError("Value must be a string")
+
+    def is_sentence(self):
+        return True if self.value.endswith('.') else False
+
+    def is_question(self):
+        return True if self.value.endswith('?') else False
+
+    def is_exclamation(self):
+      return True if self.value.endswith('!') else False
+    
+    def count_sentences():
